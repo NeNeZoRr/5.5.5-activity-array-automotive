7aa0d08b6362d3eaa41641a58f22f58f2ce7cd72
NeNeZoRr <zorrillalin@gmail.com>
2023-09-11 17:58:14 -0400

    Updated 	modified:   factory.js 	modified:   vehicle.js

--- factory.js
+++ factory.js
@@ -1,3 +1,21 @@
-class Factory{
-    
-}
+createVehicle(Vehicle) ;
+    if (Vehicle instanceof Vehicle) {
+      this.Vehicles.push(Vehicle);
+    } else {
+      console.error("Invalid vehicle object");
+    }
+  
+
+  listVehicles() ;
+    return this.Vehicles
+  
+
+  paintVehicles(vin, paintColor) ;
+    constructor (Vehicle = this.Vehicles);
+    if (vehicle) {
+      paintVehicles(paintColor);
+    } else {
+        console.log("blue");
+  }
+  
+  
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,21 @@
 class Vehicle {
-    
-}
+    constructor(make, model, year, price, vin) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.price = price;
+      this.vin = vin;
+      this.paint = undefined;
+    }
+  
+    paint(paintColor) {
+      return this.paint = paintColor;
+    }
+  
+    drive() {
+        return this.model + " " + "drive"
+    }
+  }
 
+let newFord = new Vehicle("Ford", "F-150", "2015", 19000, "1234", "blue" )
+console.log(newFord)
